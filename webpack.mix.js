b6d75f99809289f209533f8a9e6cd5d7ac783808
Mastermind <adam.gicevic@gmail.com>
2024-03-15 13:39:19 +0100

    Comment edit.

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,9 +5,8 @@ mix.js('resources/js/app.js', 'public/js')
         require('tailwindcss'),
     ])
     .copy('node_modules/tw-elements/dist/js/tw-elements.umd.min.js', 'public/js');
-    // .copy('node_modules/tw-elements/dist/js/tw-elements.umd.min.js.map', 'public/js');
 
-// Minify both JS and CSS files for production build 
+// Minify JS/CSS - production build 
 if (mix.inProduction()) {
     mix.minify();
 }
