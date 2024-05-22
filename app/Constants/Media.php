a9e6d98c70fef4a8b523cffcf792471d476d9e6a
Mastermind <adam.gicevic@gmail.com>
2024-05-22 11:58:53 +0200

    Media constants.

---
+++ app/Constants/Media.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class Media
+{
+    # Where are media files - All file root folder
+    const PATH_ROOT="images/";
+
+    # Where are media files - Awards
+    const PATH_AWARDS="images/awards/";
+}
