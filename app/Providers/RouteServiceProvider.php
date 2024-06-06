5d407c2033a006d94110df4cbd6f322a7a75db35
Mastermind <adam.gicevic@gmail.com>
2024-06-06 21:16:14 +0200

    (update) Providers v11.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,14 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+
+    /**
+     * The path to the "dashboar.account".
+     *
+     * @var string
+     */
+    public const DASH = '/dashboard/account';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
