5b5314ea3c896ed34ffe368183176a38241438c0
Mastermind <adam.gicevic@gmail.com>
2024-06-06 21:01:57 +0200

    (update) Vite switch configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const path = require('path');
+
 module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./node_modules/tw-elements/css/**/*.css",
+    "./node_modules/tw-elements/js/**/*.js"
+  ],
   theme: {
       extend: {
         fontSize: {
@@ -7,16 +15,9 @@ module.exports = {
       },
     },
   },
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
+  mode: 'jit',
+  plugins: [
+    require(path.resolve('node_modules', 'tw-elements', 'plugin.cjs'))
   ],
-  content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/tw-elements/dist/js/**/*.js"
-  ],
-  plugins: [require("tw-elements/dist/plugin.cjs")],
   darkMode: "class"
 };
-
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,49 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import copy from 'rollup-plugin-copy';
+import swellCheckMastg from './rollup-plugins/swell-check-mastg/swell-check-mastg.plugin.js';
+
+const optionsCopyToPublic = {
+  SRC : `${process.cwd()}/resources`,
+  DST    : `${process.cwd()}/public`,
+}
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
+  server: {
+    watch: {
+      include: ['resources/sass/**', 'resources/css/**', 'resources/js/**']
+    }
+  },
+  build: {
+    manifest: true,
+    outDir: './public/build',
+    emptyOutDir: true,
+    minify: false,
+    rollupOptions: {
+      input: {
+        'js/app.js': 'resources/js/app.js',
+        'css/app.css': 'resources/css/app.css',
+      },
+      plugins: [
+        swellCheckMastg,
+        copy({
+          targets: [
+            { src: `${optionsCopyToPublic.SRC}/css/app.css`, dest: `${optionsCopyToPublic.DST}/css` },
+            // { src: `${process.cwd()}/node_modules/tw-elements/css/tw-elements.min.css`, dest: `${optionsCopyToPublic.DST}/css` },
+            // { src: `${process.cwd()}/node_modules/tw-elements/js/tw-elements.umd.min.js`, dest: `${optionsCopyToPublic.DST}/js` },
+          ],
+          verbose: true,
         }),
-    ],
+      ],
+    },
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ]
 });
