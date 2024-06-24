152a597840332e27633d9d0d9e181e85e8c34966
Mastermind <adam.gicevic@gmail.com>
2024-06-25 01:08:57 +0200

    (fix) Properties type change.

--- app/ProjectApps/Quest.php
+++ app/ProjectApps/Quest.php
@@ -3,18 +3,19 @@ namespace App\ProjectApps;
 
 abstract class Quest 
 {
-    private Intiger $project_id;
-    private Intiger $user_id;
+    private int $project_id;
+    private int $user_id;
     private String $name;
     private String $created_at;
     private String $done_at;
-    private Number $score;
-    private Intiger $level;
+    private float $score;
+    private int $level;
 
-    public function __construct(String $taskTitle, Intiger $user, Intiger $project) 
+    public function __construct(String $taskTitle, int $user, int $project) 
     {
-        $super->project_id = $project;
-        $super->user_id = $user;
-        $super->name = $taskTitle;
+        // FIXME: Move all data into child class
+        $this->project_id = $project;
+        $this->user_id = $user;
+        $this->name = $taskTitle;
     }
 }
