025031f50b8ef7cf32530a0a8a89da8b1e5e09f3
Mastermind <adam.gicevic@gmail.com>
2024-07-02 23:25:16 +0200

    (fix) Clean up. Old method.

--- app/Http/Controllers/Api/FileUploadApiController.php
+++ app/Http/Controllers/Api/FileUploadApiController.php
@@ -5,27 +5,41 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 class FileUploadApiController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $uploadedFiles = $request->file(); // Get all uploaded files
 
         $filePaths = [];
         $files = [];
 
+        if (empty($uploadedFiles)) {
+            return response()->json(
+                [
+                    "message" => "Save files to disk failed.",
+                    "error" => "No file names proveded."
+                ],
+                500
+            );
+        }
+
         foreach ($uploadedFiles as $key => $file) {
+        
             $fileName = $file->store('public/images'); // Store to public/images directory
 
             $name = basename($fileName);
             $memeType = $file->getMimeType();
 
             $files[] = [
-               'name' => $name,
-               'path' => 'images/' . $name,
-               'memeType' => $memeType
+                'name' => $name,
+                'path' => 'images/' . $name,
+                'memeType' => $memeType
             ];
         }
 
-        return response()->json(['message' => 'Images uploaded successfully', 'files' => $files]);
+        return response()->json(['message' => 'Images uploaded successfully', 'files' => $files], 200);
     }
 }
